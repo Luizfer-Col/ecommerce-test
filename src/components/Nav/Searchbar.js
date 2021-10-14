07c6a9dc09d1369368f8d832b140b5fd48245023
Luizfer-Col <luizfercolorado@gmail.com>
2021-10-13 21:07:40 -0500

    add some styles to search and nav bar

--- src/components/Nav/Searchbar.js
+++ src/components/Nav/Searchbar.js
@@ -2,15 +2,13 @@ import React from "react";
 import Style from "./Searchbar.module.css";
 import { search } from "react-icons-kit/icomoon/search";
 import Icon from "react-icons-kit";
-import Logo from '../../images/logo.png'
+import Logo from '../../images/logoi.png'
+import {cart} from 'react-icons-kit/icomoon/cart'
 
 function Searchbar() {
   return (
-    <div>
-        <span className={Style.titleppal}>
-						{<img src={Logo} alt="logo" />}
-						
-					</span>
+    <div className={Style.containernav}>
+        <img style={{width:"100px"}} src={Logo} alt="logo"/>
       <div className={Style.searchBar}>
         <input
           className={Style.input}
@@ -20,7 +18,19 @@ function Searchbar() {
           <Icon icon={search} />
         </button>
       </div>
+      <span>
+          MIS PEDIDOS
+      </span>
+      <div className={Style.ingresa}>
+          <h6>HOLA ALIAD@</h6>
+          <h5>INGRESA</h5>
+      </div>
+      <div className={Style.carrito}>
+      <Icon icon={cart} size={40}/>
+      <span>CARRITO</span>
     </div>
+    </div>
+
   );
 }
 
