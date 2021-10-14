30d59faad94f616c1e21379a780b5c4c0e483a3d
Luizfer-Col <luizfercolorado@gmail.com>
2021-10-14 14:38:04 -0500

    car hover effect added

--- src/components/Vitrina/Cards.js
+++ src/components/Vitrina/Cards.js
@@ -1,50 +1,56 @@
-import React from "react";
-import Card from "./Card";
+import React, { Fragment } from "react";
 import Style from "./Cards.module.css";
+import Silla1 from "../../images/sillas/silla01.png"
+import Silla2 from "../../images/sillas/silla02.png"
+import Silla3 from "../../images/sillas/silla03.png"
+import Silla4 from "../../images/sillas/silla04.png"
 
 function Cards({ sectionTitle }) {
   return (
-    <div className={Style.cardsppal}>
-        <h2>{sectionTitle}</h2>
-      <div className={Style.containercards}>
-        <Card
-          img={
-            "https://www.farmalisto.com.co/140364/comprar-silla-de-ruedas-transporte-konfort-basic-por-unidad-precio-h01sp06953.jpg"
-          }
-          price={"250.000"}
-          text={
-            "Anim ut ex culpa adipisicing mollit anim adipisicing fugiat incididunt deserunt qui pariatur minim aliquip."
-          }
-        />
-        <Card
-          img={
-            "https://www.farmalisto.com.co/140364/comprar-silla-de-ruedas-transporte-konfort-basic-por-unidad-precio-h01sp06953.jpg"
-          }
-          price={"250.000"}
-          text={
-            "Anim ut ex culpa adipisicing mollit anim adipisicing fugiat incididunt deserunt qui pariatur minim aliquip."
-          }
-        />
-        <Card
-          img={
-            "https://www.farmalisto.com.co/140364/comprar-silla-de-ruedas-transporte-konfort-basic-por-unidad-precio-h01sp06953.jpg"
-          }
-          price={"250.000"}
-          text={
-            "Anim ut ex culpa adipisicing mollit anim adipisicing fugiat incididunt deserunt qui pariatur minim aliquip."
-          }
-        />
-        <Card
-          img={
-            "https://www.farmalisto.com.co/140364/comprar-silla-de-ruedas-transporte-konfort-basic-por-unidad-precio-h01sp06953.jpg"
-          }
-          price={"250.000"}
-          text={
-            "Anim ut ex culpa adipisicing mollit anim adipisicing fugiat incididunt deserunt qui pariatur minim aliquip."
-          }
-        />
+      <Fragment>
+  
+        <div className={Style.title}>
+
+            <h1>{sectionTitle}</h1>
+
+
       </div>
+
+    <div className={Style.container}>
+       
+    <div className={Style.card}>
+        <img src={Silla1} />
+        <h4>Naturaleza</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, excepturi unde?</p>
+        <a href="#">Leer más</a>
     </div>
+    
+    <div className={Style.card}>
+        <img src={Silla2} />
+        <h4>Naturaleza</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, excepturi unde?</p>
+        <a href="#">Leer más</a>
+    </div>
+    
+    <div className={Style.card}>
+        <img src={Silla3} />
+        <h4>Naturaleza</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, excepturi unde?</p>
+        <a href="#">Leer más</a>
+    </div>
+    
+    <div className={Style.card}>
+        <img src={Silla4} />
+        <h4>Naturaleza</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, excepturi unde?</p>
+        <a href="#">Leer más</a>
+    </div>
+    
+
+</div>
+
+</Fragment>
+
   );
 }
 
